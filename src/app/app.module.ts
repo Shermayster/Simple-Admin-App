b9d4e5231eb6f442b05686d9b897f0d3ba309bf0
pavelshermayster <sherm84@gmail.com>
2018-03-23 10:07:04 +0300

    feat: added app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { EmailEditorComponent } from './email-editor/email-editor.component';
+import { EmailDisplayComponent } from './email-display/email-display.component';
+import { LoggerComponent } from './logger/logger.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmailEditorComponent,
+    EmailDisplayComponent,
+    LoggerComponent
   ],
   imports: [
     BrowserModule
